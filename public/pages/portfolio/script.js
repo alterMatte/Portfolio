3d4005a700719ee779bbb77d9ce1e764e5334c9c
alterMatte <alter4junk@gmail.com>
2026-03-15 08:02:23 +0800

    Reintroduce project display and initialization logic
    Restored and updated initialization logic to run display6Projects along with other functions.

--- public/pages/portfolio/script.js
+++ public/pages/portfolio/script.js
@@ -1,4 +1,4 @@
-//import { display6Projects } from '../../../src/scripts/displayProject.js';
+import { display6Projects } from '../../../src/scripts/displayProject.js';
 import introClickHandler from '../../../src/scripts/introClick.js';
 import quickEmail from '../../../src/scripts/quickEmail.js';
 
@@ -8,17 +8,15 @@ const init = () => {
     console.log('this run inside');
 
     // Run functions
-    //display6Projects();
+    display6Projects();
     introClickHandler();
     quickEmail();
 };
 
 // Wait for DOM to be ready
-/*
+
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', init);
 } else {
     init();
 }
-*/
-init();
